7c6605d8dbb7a15da58b2a8c6294efd7f9351770
irawl254 <ir254@byu.edu>
2025-04-12 15:14:42 -0600

    rearranged js functions

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 async function fetchData() {
-    try {
-      const response = await fetch('https://music.is120.ckearl.com/');
-      data = await response.json(); // Assumes the site provides a JSON response
-      // displayArtists(albums,'pop');
-      myDiv=document.getElementById('genre-list'); 
-      myDiv.innerHTMLL="";
-      for(let i in data.data.spotify_top_genre_artists){
-        const myLine = document.createElement('p');
-        myLine.textContent = data.data.spotify_top_genre_artists[i].genre_name;
-        // myDiv.appendChild(myLine);        
-      }
-    } catch (error) {
-      console.error('Error fetching album data:', error);
-    }
+  try {
+    alert("Fetching data...");
+    const response = await fetch("https://music.is120.ckearl.com/");
+    globalThis.data = await response.json(); // Assumes the site provides a JSON response
+    // displayArtists(albums,'pop');
+  } catch (error) {
+    console.error("Error fetching album data:", error);
   }
+}
+function loadGenres() {
+  myDiv = document.getElementById("genre-select");
+  myDiv.innerHTMLL = "";
+  for (let i in globalThis.data.data.spotify_top_genre_artists) {
+    alert(globalThis.data.data.spotify_top_genre_artists[i].genre_name);
+    const myLine = document.createElement("option");
+    myLine.textContent =
+      globalThis.data.data.spotify_top_genre_artists[i].genre_name;
+    myLine.value = globalThis.data.data.spotify_top_genre_artists[i].genre_name;
+    myDiv.appendChild(myLine);
+  }
+}
 
-   async function loadPage(pageName, divName) {
-    alert("Loading page: " + pageName);
-    const pageDiv = document.getElementById(divName);
-    pageDiv.innerHTML = ''; // Clear the div before loading new content
-    const pagePath = `./${pageName}`; // Assuming the HTML files are in the same directory
-    fetch(pagePath)
-      .then(response => response.text())
-      .then(data => {
-        pageDiv.innerHTML = data;
-      })
-      .catch(error => console.error('Error loading page:', error));
-   }
+async function loadPage(pageName, divName) {
+  const pageDiv = document.getElementById(divName);
+  pageDiv.innerHTML = ""; // Clear the div before loading new content
+  const pagePath = `./${pageName}`; // Assuming the HTML files are in the same directory
+  fetch(pagePath)
+    .then((response) => response.text())
+    .then((data) => {
+      pageDiv.innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading page:", error));
+}
+
+fetchData();
